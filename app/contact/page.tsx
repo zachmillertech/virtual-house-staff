b06ba87e7da1e1720273d3a84b895967aaa61661
zachmillertech <account@zachmiller.tech>
2026-03-27 09:54:11 -0400

    Updated contact page
    added address

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -32,6 +32,15 @@ export default function Contact() {
                 https://gloriamkardongmd.com
               </a>
             </p>
+            <p className="text-xl font-semibold text-purple-700">
+3000 El Camino Real < /br>
+4 Palo Alto Square< /br>
+Building 4 Suite 200< /br>
+Palo Alto, CA 94306< /br>
+            </p>
+
+
+            
           </div>
         </div>
       </div>
