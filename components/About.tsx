5a2bd65833a8ba9e835424c2cdea6cd8d61171e5
zachmillertech <account@zachmiller.tech>
2026-03-31 11:55:04 -0400

    Update title in About component
    DFAPA to DLFAPA
    added DABPN

--- components/About.tsx
+++ components/About.tsx
@@ -4,7 +4,7 @@ export default function About() {
   return (
     <section className="py-16">
       <div className="container mx-auto">
-        <h2 className="text-3xl font-bold text-center text-purple-600 mb-12">About Gloria M Kardong MD DFAPA</h2>
+        <h2 className="text-3xl font-bold text-center text-purple-600 mb-12">About Gloria M Kardong MD, DLFAPA, DABPN</h2>
         <div className="grid grid-cols-1 md:grid-cols-2 gap-8 items-center">
           <div>
 
